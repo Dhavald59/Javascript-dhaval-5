cc45ac6ebbc5933c722c3db551cb30e8f24933ae
Dhavald59 <desaidhaval2004@gmail.com>
2024-12-03 19:03:31 +0530

    for loop 3/12/24

---
+++ forloop.js
@@ -0,0 +1,72 @@
+// for loop 
+// let i;
+// for ( i = 1; i < 6; i++){
+//     console.log(i);
+// }
+// console.log("i ===>", i);
+
+
+// pogram 1 
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// pogram2
+
+// for (let i = 51; i <= 79; i++){
+//     console.log(i);
+// }
+
+// pogram3 
+
+// let n;
+// n = Number(prompt('enter your number'))
+// for (i = 1 ; i <= n ; i++) {
+//     console.log(i);
+//  }
+
+// pogram4
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= 1 ; i++) {
+//     console.log(i);
+// }
+
+// pogram5
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= n ; i++) {
+//     console.log(i);
+// }
+
+// pogram6
+
+// let n = Number(prompt('enter your number'));
+// for (i = 1; i <= n; i+=2) {
+//     console.log(i);
+// }
+
+// pogram7
+
+// let n = Number(prompt('enter your number'));
+// for (i = 0; i <= n; i+=2) {
+//     console.log(i);
+// }
+
+// pogram8
+// for (i = 1; i <= 5; i++) {
+//     if (i%2 == 0){
+//         console.log(i**2);
+//     }
+//     if (i%2 == 1){
+//         console.log(i);
+//     }
+// }
+// pogram9
+
+// for(i=0;i<=10;i++){
+//     if(i+=i){
+//         console.log(i)
+//     }
+// }
