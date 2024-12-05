ffaa91b22af5b1c19c5268d522017274a1b841a2
Dhavald59 <desaidhaval2004@gmail.com>
2024-12-05 18:44:54 +0530

    project 3  5/12/24

---
+++ project3.js
@@ -0,0 +1,85 @@
+// pogram 1  : Write JS Program to check number is Palindrome or not 
+
+// let n = Number(prompt('enter your number'));
+// let value = n;
+// let reversnumber = 0;
+// while (n > 0) {
+//     let number = n % 10;
+//     reversnumber = reversnumber * 10 + number;
+//     n = (n - (n % 10) ) / 10;
+// }
+// if (value === reversnumber) {
+//     console.log('palindrome number ',value)
+// }
+// else {
+//     console.log('not a palindrome number ',value)
+// }
+
+// output :  palindrome number 121
+
+
+/////////////////////////////////////////////////////////
+
+
+// pogram 2  : Print Series 1, 4, 3, 16, 5, ....
+// for (i = 1; i <= 5; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i ** 2);
+//     }
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+// output : 1 4 3 16 5
+
+
+///////////////////////////////////////////////////////////
+
+
+// pogram 3 : Print Multiplication table of n
+// let i =1 ;
+// let n = Number(prompt('enter your number'));
+// while (i<=10){
+//     i++;
+//     console.log(n + "*" + i + "=", n * i);
+
+// }
+
+// output : 10 * 1 = 10
+
+
+/////////////////////////////////////////////////////////
+
+
+// pogram 4 : find out sum of digit of given number
+// let n = Number(prompt("enter your number"));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+//     console.log(sum);
+
+// }
+
+// output :- 1,3,6,10,15,21
+
+
+//////////////////////////////////////////////////////////
+
+
+// pogram 5 : print series 1, 1, 2, 3, 5, 8, ...
+
+// let a = 1, b = 1 , i = 0;
+// console.log(a);
+// console.log(b);
+// while (i <= 5) {
+//     let c = a + b;
+//     i++;
+//     console.log(c);
+//     a = b;
+//     b = c;
+// }
+
+
+// output :- 1 , 1 , 2 , 3 , 5 , 8 , 21
