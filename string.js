56974db406dcdff2075a050bead72ca0e35adc73
Dhavald59 <desaidhaval2004@gmail.com>
2024-12-17 18:52:57 +0530

    string 17-12-24

---
+++ string.js
@@ -0,0 +1,36 @@
+// // string =>  collection of string
+
+// let a = "Hello"
+// let data = "Hello world"
+// console.log(a);
+// console.log(data.length);
+// console.log(typeof data);
+
+// console.log(`javascript ${20+1} "red white's "`);
+
+// let data = "           JavaScript and Java are completely different languages, both in concept and design";
+
+// let a = data.slice(0,15);
+
+// let a = data.substring(10,15);
+
+// let a = data.split("java");
+
+// let a = data.toUpperCase("java");
+
+// let a = data.trim("");
+
+// console.log(a.length)
+
+
+// let a = Math.floor(15.6)
+// let a = Math.ceil(15.6)
+// let a = Math.round(15.6)
+// let a = Math.floor(Math.random())
+// console.log(a)
+
+///////////////////////////////////////////////////////////////
+
+let date = new Date(2030)
+console.log(date)
+
