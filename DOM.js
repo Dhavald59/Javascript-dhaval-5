f4e7d22673d6716455966c5c766cc485e7afc3c1
Dhavald59 <desaidhaval2004@gmail.com>
2024-12-25 18:53:41 +0530

    Dom 25/12/24

---
+++ DOM.js
@@ -0,0 +1,33 @@
+// DOM :- Documnet object model
+
+// document.getElementById
+// document.getElementsByTagName
+// document.getElementsByClassName
+// querysector & querysectorAll
+
+// id 
+// let h2 = document.getElementById('name')
+// console.log(h2)
+// h2.innerHTML = "javascript"
+// h2.style.color = "red"
+// h2.setAttribute("class", "demo")
+
+// tag name
+// let data = document.getElementsByTagName('p')
+// console.log(data)
+// data[0].style.color = "red"
+
+
+// class name 
+
+// let data = document.getElementsByClassName("handle")
+// console.log(data);
+// data.innerHTML = "javascript"
+
+// querysector & querysectorAll
+
+// let data = document.querySelector('h2')
+// console.log(data);
+let data = document.querySelectorAll('h2')
+console.log(data);
+
