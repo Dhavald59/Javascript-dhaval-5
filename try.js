f9a4dcaa642be5bb6496e0501d6f90cbed430af5
Dhavald59 <desaidhaval2004@gmail.com>
2024-12-30 19:04:24 +0530

    Error Throw and try to catch 30/12/24

---
+++ try.js
@@ -0,0 +1,11 @@
+try {
+    let a = 10;
+    // let b = 5;
+    if(b == undefined){
+        throw new error("hello")
+    }
+    console.log(a/b);
+    
+} catch (error) {
+    console.log("error")
+}
