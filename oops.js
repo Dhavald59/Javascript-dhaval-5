76b2dd3a6b3b70ae37c74a08051118a51d88133c
Dhavald59 <desaidhaval2004@gmail.com>
2025-01-01 11:43:41 +0530

    class and object and new keyword 1/1/25

---
+++ oops.js
@@ -0,0 +1,11 @@
+class testing {
+    sayhello() {
+        console.log("Hello");
+    }
+    gretin(){
+        console.log("Goodbye");
+    }
+}
+let obj = new testing();
+obj.sayhello(); // Outputs: Hello
+obj.gretin(); // Outputs: Goodbye
