7734a8b760ace2324b340ebb0e679cb6b9b0ad76
Dhavald59 <desaidhaval2004@gmail.com>
2025-01-22 18:43:15 +0530

    Edit form and delete 22/1/25

--- crad.js
+++ crad.js
@@ -1,9 +1,17 @@
+let addForm = document.getElementById('addData');
+let editForm = document.getElementById('editData');
+
+let title = document.querySelector('#bookname')
+let price = document.querySelector('#price')
+let category = document.querySelector('#category')
+let author = document.querySelector('#author')
+
+let editTitle = document.querySelector('#editbookname')
+let editPrice = document.querySelector('#editprice')
+let editCategory = document.querySelector('#editcategory')
+let editAuthor = document.querySelector('#editauthor')
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
-    let title = document.querySelector('#bookname')
-    let price = document.querySelector('#price')
-    let category = document.querySelector('#category')
-    let author = document.querySelector('#author')
     let oldData = JSON.parse(localStorage.getItem('book')) || []
     let newBook = {
         title: title.value,
@@ -22,15 +30,38 @@ document.querySelector('form').addEventListener('submit', (e) => {
 function loadData() {
     let books = JSON.parse(localStorage.getItem('book'))
     let result = ""
-    books.forEach(book, index => {
-        row.innerHTML = `<tr>
+    books.forEach((book, index) => {
+        row = `<tr>
         <td>${index + 1}</td>
-        <td>${index + 1}</td>
-        <td>${index + 1}</td>
-        <td>${index + 1}</td>
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td>${book.category}</td>
+        <td>${book.author}</td>
+        <td><button onclick="EditRec(${index})">Edit</button> || <button onclick="DeleteRec(${index})">Delete</button></td>
         </tr>`
         result = result + row;
     });
-    document.querySelector('#bookView').innerHTml = result;
+    document.querySelector('#bookView').innerHTML = result;
 }
 loadData()
+
+function DeleteRec(index) {
+    let allData = JSON.parse(localStorage.getItem('book'));
+    allData.splice(index, 1);
+    localStorage.setItem('book', JSON.stringify(allData));
+    loadData()
+}
+
+function EditRec(index) {
+    let allData = JSON.parse(localStorage.getItem('book'));
+    let editRecord = allData[index]
+
+
+    addForm.style.display = "none"
+    editForm.style.display = "block"
+
+    editTitle.value = editRecord.title;
+    editPrice.value = editRecord.price;
+    editCategory.value = editRecord.category;
+    editAuthor.value = editRecord.author;
+}
